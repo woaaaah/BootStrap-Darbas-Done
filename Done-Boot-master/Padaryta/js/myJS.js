83648d4c6bb222e8013a96d4dd96d857770094aa
Dovydas <55572432+woaaaah@users.noreply.github.com>
2019-11-07 10:56:09 +0200

    Add files via upload

---
+++ Done-Boot-master/Padaryta/js/myJS.js
@@ -0,0 +1,39 @@
+//carousel
+
+var $item = $('.carousel .item'); 
+var $wHeight = $(window).height();
+$item.eq(0).addClass('active');
+$item.height($wHeight); 
+$item.addClass('full-screen');
+
+$('.carousel img').each(function() {
+  var $src = $(this).attr('src');
+  var $color = $(this).attr('data-color');
+  $(this).parent().css({
+    'background-image' : 'url(' + $src + ')',
+    'background-color' : $color
+  });
+  $(this).remove();
+});
+
+$(window).on('resize', function (){
+  $wHeight = $(window).height();
+  $item.height($wHeight);
+});
+
+$('.carousel').carousel({
+  interval: 6000,
+  pause: "false"
+});
+
+ //pagescroll2id
+$(document).ready(function() {
+    $("a[rel='m_PageScroll2id']").mPageScroll2id({
+      
+    });
+    // mixitup
+    var containerEl = document.querySelector('.mixContainer');
+    var mixer = mixitup(containerEl);
+});
+
+
